d78b6ba0f0fc54f57c21a6072d83c8acc050313a
blinditdev <dustinmarkinc@gmail.com>
2024-08-17 15:27:12 -0400

    added copyright info

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
+/*
+Copyright (c) 2024 Dustin Lewis and Mark Opatow. All rights reserved.
+This work is licensed under the Creative Commons Attribution-NonCommercial 4.0 International License.
+To view a copy of this license, visit http://creativecommons.org/licenses/by-nc/4.0/
+or send a letter to Creative Commons, PO Box 1866, Mountain View, CA 94042, USA.
+You may not use this file except in compliance with this license. Commercial use is prohibited without prior authorization.
+*/
+
 document.addEventListener('DOMContentLoaded', function() {
     const flashcards = [
         { question: "Why might users receive errors when populating a custom picklist field in Salesforce?", answer: "Due to contradicting validation rules." },
